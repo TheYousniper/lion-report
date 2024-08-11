70b16f23d581b24c69a27df2777e2217fcfb2b36
johalama2023 <johalama2023@gmail.com>
2024-08-11 16:43:18 -0500

    actualizando

--- src/pages/Contactanos.jsx
+++ src/pages/Contactanos.jsx
@@ -27,12 +27,13 @@ const Contactanos = () => {
         <div className="inset-0 flex justify-center items-start z-20">
           <div className="lg:w-[70%] lg:h-[200px] w-[95%] mt-5 flex flex-col justify-center items-center rounded-[32px] bg-lion-blue">
             <h1 className="font-helvetic-bold lg:text-4xl text-base p-3 text-center uppercase text-lion-yellow">
-              Si tienes algo que reportar o alguna idea que nos pueda ayudar a reducir o mitigar la pobreza en el Perú, no dudes en contactarnos.
+              Si tienes algo que reportar o alguna idea que nos pueda ayudar a
+              reducir o mitigar la pobreza en el Perú, no dudes en contactarnos.
             </h1>
           </div>
         </div>
         <div className="max-w-lg mx-auto my-7 p-6 bg-lion-blue rounded-lg shadow-lion-yellow">
-          <form>
+          <form ref={form} onSubmit={sendEmail}>
             <div className="mb-4">
               <label
                 htmlFor="name"
@@ -43,7 +44,7 @@ const Contactanos = () => {
               <input
                 type="text"
                 id="name"
-                name="name"
+                name="user_name"
                 className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
               />
             </div>
@@ -57,7 +58,7 @@ const Contactanos = () => {
               <input
                 type="email"
                 id="email"
-                name="email"
+                name="user_email"
                 className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
               />
             </div>
@@ -78,7 +79,7 @@ const Contactanos = () => {
             <div className="text-center">
               <button
                 type="submit"
-                className="px-6 py-2 bg-lion-yellow text-lion-blue font-helvetic-medium text-xl rounded-md shadow   focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                className="px-6 py-2 bg-lion-yellow text-lion-blue font-helvetic-medium text-xl rounded-md shadow transform hover:scale-110 transition-transform focus:outline-none"
               >
                 Enviar
               </button>
